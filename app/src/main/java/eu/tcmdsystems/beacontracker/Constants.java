73efa63e74087852fd611342bb7c925f94ab2441
Mihai Csaky <mihai.csaky@honeywell.com>
2017-09-25 14:29:47 +0300

    beacon layout as constants

--- app/src/main/java/eu/tcmdsystems/beacontracker/Constants.java
+++ app/src/main/java/eu/tcmdsystems/beacontracker/Constants.java
@@ -7,6 +7,15 @@ class Constants {
 
     private Constants() {
     }
+
+
+    //Beacon	Layout
+    static final String ALTBEACON_LAYOUT="m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";
+    static final String EDDYSTONE_TLM_LAYOUT="x,s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15";
+    static final String EDDYSTONE_UID_LAYOUT="s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19";
+    static final String EDDYSTONE_URL_LAYOUT="s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v";
+    static final String IBEACON_LAYOUT="m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+
     static final String BTLE_SCAN = "eu.tcmdsystems.beacontracker.action.SCAN";
     /**
      * Eddystone-UID frame type value.
